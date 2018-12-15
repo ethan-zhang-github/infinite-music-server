00a68e940eabdee031b5988aef04e1aa351da0ee
JUST1984 <yixiezi1994@gmail.com>
2018-12-15 22:03:54 +0800

    project init

---
+++ src/main/java/com/just1984/music/InfiniteMusicServerApplication.java
@@ -0,0 +1,14 @@
+package com.just1984.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfiniteMusicServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfiniteMusicServerApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/just1984/music/InfiniteMusicServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.just1984.music;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InfiniteMusicServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
